b550e8d4cbae3d0a04c6cbeb1add6a7e9b2e7fac
seokheejang <shjang@dkargo.io>
2024-04-12 18:00:10 +0900

    [contract] clone init core code by eth-infinitism

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,8 +1,23 @@
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-etherscan";
+
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: { enabled: true, runs: 1000000 },
+        },
+      },
+    ],
+  },
 };
 
 export default config;
